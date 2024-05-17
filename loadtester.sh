b2bf54f31e890f63e9ad317b82156dbaf44002fa
Markus Siegert <markus.siegert@telekom.de>
2024-05-17 16:16:05 +0200

    your mom

--- loadtester.sh
+++ loadtester.sh
@@ -2,38 +2,43 @@
 # TODO parallel and sequential mode
 # TODO time limit
 
-if [ ! $# -eq 3 ]; then
-    echo "The program requires 3 arguments."
-    exit 1
-fi
+# if [ ! $# -eq 3 ]; then
+#     echo "The program requires 3 arguments."
+#     exit 1
+# fi
+
+#!/bin/bash
 
 url=$1
-
 num_parallel_threads=$2
-
 requests_per_thread=$3
-
-variable_array=$4
+variable_array=(${@:4})
 
 total_requests=$(($num_parallel_threads * $requests_per_thread))
-
 tempfile=$(mktemp)
 
 function perform_requests() {
-    for ((i=1; i<=requests_per_thread; i++)); do
-        (( $variable_array > 0 )) && url="${url}${variable_array[i-1]"
-        curl -s -w ",%{time_total},%{size_download},%{http_code}\n" $url >> $tempfile &
-        (( $i % num_parallel_threads == 0 )) && wait
+    for ((i=0; i<requests_per_thread; i++)); do
+        current_url=$url
+        if [ ${#variable_array[@]} -gt 0 ]; then
+            current_url="${current_url}${variable_array[i % ${#variable_array[@]}]}"
+        fi
+        curl -s -w ",%{time_total},%{size_download},%{http_code}\n" $current_url >> $tempfile &
+        if (( (i + 1) % num_parallel_threads == 0 )); then
+            wait
+        fi
     done
+    wait
 }
 
 echo "Starting load test..."
-for ((thread=1; thread<=num_parallel_threads; thread++))
+for ((thread=0; thread<num_parallel_threads; thread++))
 do
-    (perform_requests) &
+    perform_requests &
 done
 
 wait
+
 echo "Load test completed."
 
 awk -F, '{
