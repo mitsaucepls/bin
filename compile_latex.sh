8010fb27bd083e69024d2dad9ae600864b8e00a3
Markus Siegert <spongebobmarkus@gmail.com>
2024-09-10 16:23:23 +0200

    ci: new compile_latex logic

--- compile_latex.sh
+++ compile_latex.sh
@@ -5,7 +5,42 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-pdflatex --shell-escape $1.tex
-bibtex $1
-pdflatex --shell-escape $1.tex
-pdflatex --shell-escape $1.tex
+engine=pdflatex
+
+skip_bibtex=0
+
+for arg in "$@"; do
+    case "$arg" in
+    --skip-bibtex)
+        skip_bibtex=1
+        ;;
+    --engine=*)
+        engine="${arg#*=}"
+        ;;
+    esac
+done
+
+case "$engine" in
+pdfTeX|pdflatex)
+    engine="pdflatex --shell-escape"
+    ;;
+XeTeX|xelatex)
+    engine="xelatex --shell-escape"
+    ;;
+LuaTeX|lualatex)
+    engine="lualatex --shell-escape"
+    ;;
+*)
+    echo "Invalid engine specified. Use one of: pdfTeX, XeTeX, LuaTeX"
+    exit 2
+    ;;
+esac
+
+$engine $1.tex
+
+if [ "$skip_bibtex" -eq 0 ]; then
+    bibtex $1
+    $engine $1.tex
+fi
+
+$engine $1.tex
